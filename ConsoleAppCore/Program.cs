25fa16e8b6259bb73b49c6ae734dfd0e9f1641b6
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-01-15 11:54:41 +0100

    Added new massage

--- ConsoleAppCore/Program.cs
+++ ConsoleAppCore/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleAppCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("New Commit added");
+
             Console.ReadKey();
         }
     }
